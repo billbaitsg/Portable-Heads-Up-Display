e930119cad8af3ab253ace6e91da794d2bc63562
Bill Bai <bill@billbai.com>
2011-01-30 08:19:39 -0500

    Copied ATTiny-5 code from RNG block over to AMB.  Modified AMB files to conform to their block.

---
+++ AMB/main.c
@@ -0,0 +1,43 @@
+/***********************************************************
+Bill Bai
+Purdue ECET
+Senior Project
+Portable Heads Up Display
+
+	Block:	Ambient Light
+	
+	Module:	Main program
+	
+	Desc.:	This is the main program section of the 
+			Ambient Light microcontroller
+**********************************************************/
+
+#include "proj_hdr.h"	/* Include project header file */
+
+/* Global Variables for ADC readings */
+volatile char LIGHT = 0;	/* Ambient Light value */
+
+int main(void)7
+{
+	/* Local Variables */
+	unsigned char cmd = 0;
+	
+	/* Initialize software modules */
+	adc_init();				/* Initialize ADC */
+	USI_SPI_initslave();	/* Initialize SPI as slave */
+	
+	sei();	/* Enable global interrupts */
+	
+	while(1)
+	{
+		cmd = USI_SPI_getc();
+		USI_SPI_wait();
+		if( cmd == 'L' )	/* if the X axis is requested */
+		{
+			USI_SPI_putc(LIGHT);	// Send temp value to SPI and increment
+			USI_SPI_wait();		// wait for transmission to finish
+		}
+		else{}	/* all other requests, do nothing */
+	}	/* End of while */
+	return 0;
+}	/* End of main */
