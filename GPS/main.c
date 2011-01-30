0c1e62f23213dc1f40077cd487d012c3edfab40a
Bill Bai <bill@billbai.com>
2011-01-30 08:29:37 -0500

    Copied USI module to GPS block.  Modified files copied to GPS block to correspond to that block.

---
+++ GPS/main.c
@@ -0,0 +1,50 @@
+/***********************************************************
+Bill Bai
+Purdue ECET
+Senior Project
+Portable Heads Up Display
+
+	Block:	GPS
+	
+	Module:	Main program
+	
+	Desc.:	This is the main program section of the 
+			Ambient Light microcontroller
+**********************************************************/
+
+#include "proj_hdr.h"	/* Include project header file */
+
+int main(void)
+{
+	/* Local Variables */
+	unsigned char cmd = 0;
+	
+	/* Initialize software modules */
+	USI_SPI_initslave();	/* Initialize SPI as slave */
+	
+	sei();	/* Enable global interrupts */
+	
+	while(1)
+	{
+		cmd = USI_SPI_getc();
+		if( cmd == 'H' ) 		/* Heading requested */
+		{
+		
+		}
+		else if( cmd == "V" )	/* Speed requested (Velocity) */
+		{
+		
+		}
+		else if( cmd == "A" )	/* Altitude requested */
+		{
+		
+		}
+		else if( cmd == "T" )	/* Time requested */
+		{
+		
+		}
+		else					/* Ignore all other requests */
+		{}	/* End of if */
+	}	/* End of while */
+	return 0;
+}	/* End of main */
---
+++ GPS/proj_hdr.h
@@ -0,0 +1,26 @@
+/***********************************************************
+Bill Bai
+Purdue ECET
+Senior Project
+Portable Heads Up Display
+
+	Block:	GPS
+
+	Module:	Project header
+	
+	Desc.:	This file contains information useful to all
+			modules in this block, namely global variables
+			and include files
+**********************************************************/
+
+#ifndef PROJ_HDR_H
+#define PROJ_HDR_H
+
+/* Include Files */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+/* Module Includes */
+#include "usi.h"
+
+#endif
